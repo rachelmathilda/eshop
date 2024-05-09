2b776c15ea5d0ff16cc9fabd9f0a80aa49361bd6
Rachel-sustainabletech <rachel.mathilda@ui.ac.id>
2024-05-09 17:00:06 +0700

    Add CarRepository.java

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
@@ -8,4 +8,36 @@ public class Car {
     private String carName;
     private String carColor;
     private int carQuantity;
+
+    public String getCarId() {
+        return carId;
+    }
+
+    public void setCarId(String carId) {
+        this.carId = carId;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public void setCarName(String carName) {
+        this.carName = carName;
+    }
+
+    public String getCarColor() {
+        return carColor;
+    }
+
+    public void setCarColor(String carColor) {
+        this.carColor = carColor;
+    }
+
+    public int getCarQuantity() {
+        return carQuantity;
+    }
+
+    public void setCarQuantity(int carQuantity) {
+        this.carQuantity = carQuantity;
+    }
 }
