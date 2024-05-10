95780be8ed24c5d326f90e5bfb1e5f3280cc9251
Rachel-sustainabletech <rachel.mathilda@ui.ac.id>
2024-05-11 05:04:23 +0700

    [RED] Add tests for OrderRepository

--- src/main/java/enums/OrderStatus.java
+++ src/main/java/enums/OrderStatus.java
@@ -1,5 +1,7 @@
 package enums;
 
+import lombok.Getter;
+
 @Getter
 public enum OrderStatus {
     WAITING_PAYMENT("WAITING_PAYMENT"),
--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
@@ -1,9 +1,16 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
+import enums.OrderStatus;
+import id.ac.ui.cs.advprog.eshop.controller.OrderRepository;
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class OrderRepositoryTest {
     OrderRepository orderRepository;
