4768ebb3d573e20d1a7facee33662ddcb900e47a
Rachel Mathilda <rachel.mathilda@ui.ac.id>
2025-02-10 15:26:06 +0700

    fix bug

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -8,4 +8,28 @@ public class Product {
     private String productId;
     private String productName;
     private int productQuantity;
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String id) {
+        productId = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String name) {
+        productName = name;
+    }
+
+    public int getProductQuantity() {
+        return productQuantity;
+    }
+
+    public void setProductQuantity(int quantity) {
+        productQuantity = quantity;
+    }
 }
