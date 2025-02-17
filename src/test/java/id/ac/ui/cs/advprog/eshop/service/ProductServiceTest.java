13e64c8f82d47dab739358380ddc496c8930ac8a
Rachel Mathilda <rachel.mathilda@ui.ac.id>
2025-02-17 12:37:24 +0700

    update service unit test until 100% code coverage

--- src/test/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceTest.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.Iterator;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -50,12 +51,41 @@ class ProductServiceTest {
     @Test
     void testUpdateProduct() {
         Product product = new Product();
+        product.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        product.setProductName("Sampo Cap Bambang");
+        product.setProductQuantity(100);
+        productService.create(product);
 
+        Product updatedProduct = new Product();
+        updatedProduct.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        updatedProduct.setProductName("Sampo Cap Usep");
+        updatedProduct.setProductQuantity(50);
+        when(productRepository.update(updatedProduct)).thenReturn(updatedProduct);
+        productService.update(updatedProduct);
+
+        when(productRepository.findById(product.getProductId())).thenReturn(updatedProduct);
+        Product retrievedProduct = productService.findById("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        assertEquals(updatedProduct.getProductName(), retrievedProduct.getProductName());
+        assertEquals(updatedProduct.getProductQuantity(), retrievedProduct.getProductQuantity());
     }
 
     @Test
     void testDeleteProduct() {
         Product product = new Product();
+        product.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        product.setProductName("Sampo Cap Bambang");
+        product.setProductQuantity(100);
+        List<Product> productList = List.of(product);
+        when(productRepository.findAll()).thenReturn(productList.iterator());
+        productService.create(product);
 
+        List<Product> retrievedProductList = productService.findAll();
+        assertFalse(retrievedProductList.isEmpty());
+
+        doNothing().when(productRepository).delete(product);
+        productService.delete(product);
+
+        List<Product> deletedProductList = productService.findAll();
+        assertTrue(deletedProductList.isEmpty());
     }
 }
