ad9b6b2f9c86ebd4ccca7b62724453494cac6c4a
Rachel Mathilda <rachel.mathilda@ui.ac.id>
2025-03-04 14:21:03 +0700

    [RED] Add Order model skeleton

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -6,33 +6,19 @@ import lombok.Setter;
 import java.util.List;
 import java.util.UUID;
 
-@Getter @Setter
+@Builder
+@Getter
 public class Order {
-    private String id;
-    private List<Product> products;
-    private Long orderTime;
-    private String author;
-    private String status;
-
-    public enum status {
-        WAITING_PAYMENT,
-        FAILED,
-        CANCELLED,
-        SUCCESS
-    }
+    String id;
+    List<Product> products;
+    Long orderTime;
+    String author;
+    @Setter
+    String status;
 
     public Order(String id, List<Product> products, Long orderTime, String author) {
-        this.id = (id != null) ? id : UUID.randomUUID().toString();
-        setProducts(products);
-        this.orderTime = orderTime;
-        setAuthor(author);
     }
 
     public Order(String id, List<Product> products, Long orderTime, String author, String status) {
-        this.id = (id != null) ? id : UUID.randomUUID().toString();
-        setProducts(products);
-        this.orderTime = orderTime;
-        setAuthor(author);
-        setStatus(status);
     }
 }
