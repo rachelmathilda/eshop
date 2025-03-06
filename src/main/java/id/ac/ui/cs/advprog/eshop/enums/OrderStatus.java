fccc532dbd6eb5daa4ac476b04009aa5f72e58aa
Rachel Mathilda <rachel.mathilda@ui.ac.id>
2025-03-06 08:16:26 +0700

    [REFACTOR] Add OrderStatus enum

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/enums/OrderStatus.java
@@ -0,0 +1,22 @@
+package id.ac.ui.cs.advprog.eshop.enums;
+
+public enum OrderStatus {
+    WAITING_PAYMENT("WAITING_PAYMENT"),
+    FAILED("FAILED"),
+    SUCCESS("SUCCESS"),
+    CANCELLED("CANCELLED");
+
+    private final String value;
+    private OrderStatus(String value) {
+        this.value 
+    }
+
+    public static boolean contains(String param) {
+        for (OrderStatus orderStatus : OrderStatus.values()) {
+            if (orderStatus.name().equals(param)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
