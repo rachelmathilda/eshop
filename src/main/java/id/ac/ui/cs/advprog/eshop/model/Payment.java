882d7970078618f0d5e885ea71f70b26a39e2e18
Rachel Mathilda <rachel.mathilda@ui.ac.id>
2025-03-07 18:21:45 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -14,8 +14,22 @@ public class Payment {
     Map<String, String> paymentData;
 
     public Payment(String id, String method) {
+        this.id = id;
+        this.method = method;
     }
 
     public Payment(String id, String method, String status) {
+        this.id = id;
+        this.method = method;
+        setStatus(status);
+    }
+
+    public void setStatus(String status) {
+        String[] statusList = {"SUCCESS", "REJECTED"};
+        if (Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.status = status;
+        }
     }
 }
