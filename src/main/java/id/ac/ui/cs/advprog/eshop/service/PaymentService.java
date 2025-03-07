ca8ed3b80f06262f9149cc0e52d32900a008f9fd
Rachel Mathilda <rachel.mathilda@ui.ac.id>
2025-03-07 19:46:58 +0700

    [RED] Add PaymentService and PaymentServiceImpl skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentService.java
@@ -0,0 +1,10 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+
+public interface PaymentService {
+    public Payment addPayment(Order order, String method, Map<String, String> paymentData);
+    public Payment setStatus(Payment payment, String status);
+    public Payment getPayment(String paymentId);
+    public Payment getAllPayments();
+}
